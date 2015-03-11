166611be894a22d755fe1ca1f8ceacef7d495540
jplourd <jeffplourd@gmail.com>
2015-03-11 11:54:15 -0700

    Figured out how to add map.

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ var placeModel = [
 	}
 ];
 
-
+/*
 //I need to make a component that renders my map
 ko.components.register('some-component-name', {
     viewModel: function(params) {
@@ -23,6 +23,28 @@ ko.components.register('some-component-name', {
     },
     template: "<h2 data-bind='text: test'></h2>"
 });
+*/
+
+//I'm going to make a custom binding that will display my map.
+
+ko.bindingHandlers.map = {
+
+     init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var mapObj = valueAccessor();
+        var mapObjUnwrapped = ko.unwrap(mapObj);
+
+        var latLng = new google.maps.LatLng(
+            ko.unwrap(mapObjUnwrapped.lat),
+            ko.unwrap(mapObjUnwrapped.lng));
+
+        var mapOptions = {
+        	center: latLng,
+        	zoom: 5,
+        };
+
+        mapObjUnwrapped.googleMap = new google.maps.Map(element, mapOptions);
+    }
+};
 
 //class of data that will take in an object from the model, and represent the "places" on the map and in the list
 
@@ -43,7 +65,10 @@ var ViewModel = function() {
 		self.placeList.push(new Place(currentPlace));
 	});
 
-	self.testing = ko.observable("Bill");
+	self.myMap = ko.observable({
+        lat: ko.observable(55),
+        lng: ko.observable(11)
+    });
 }
 
 ko.applyBindings(new ViewModel());
